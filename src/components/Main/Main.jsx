ccaacdc44a7e566fd2d26e5a88cff1353d286149
lxrwar <lxrwar@gmail.com>
2023-12-10 18:31:18 +0200

    Add font

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -4,6 +4,8 @@ import styled from "styled-components";
 const MainContainer = styled.div`
     display: flex;
     flex-direction: column;
+    font-family: 'Bungee Spice', sans-serif;
+    font-size: 40px;
 `
 
 const Main = () => {
