99b19b343edd0bc8ea06329648b5ae3f0f273f7c
lxrwar <lxrwar@gmail.com>
2023-12-13 12:54:51 +0200

    Add Router and Route in App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import './App.css';
 import styled from 'styled-components';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Main from './components/Main/Main';
 import Header from './components/Header/Header';
 import CartStructure from './components/Cart/CartStructure';
 import BuyTogether from './components/BuyTogether/BuyTogether';
 import ListStructure from './components/ListProduct/ListStructure';
 import Footer from './components/Footer/Footer';
+import Login from './components/Login/Login';
 
 const MainContainer = styled.div`
   display: flex;
@@ -15,14 +16,17 @@ const MainContainer = styled.div`
 
 function App() {
   return (
-    <MainContainer>
-      <Header/>
-      <Main/>
-      <CartStructure/>
-      <BuyTogether/>
-      <ListStructure/>
-      <Footer/>
-    </MainContainer>
+    <Router>
+      <MainContainer>
+        <Header/>
+        <Route exact path="/" component={Main} />
+        <Route path="/login" component={Login} />
+        <CartStructure/>
+        <BuyTogether/>
+        <ListStructure/>
+        <Footer/>
+      </MainContainer>
+    </Router>
   );
 }
 
